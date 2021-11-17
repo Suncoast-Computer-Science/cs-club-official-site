b39624262f85949627917d1d88a10de4dbd54523
Rohit Dasgupta <rdg922@gmail.com>
2021-11-16 23:11:46 -0500

    Add WIP Backend POST Req and move GET Request logci to function

--- functions/index.js
+++ functions/index.js
@@ -37,51 +37,89 @@ admin.initializeApp({
 // };
 // app.use(authenticate);
 
-const getUserSubmissionTestcases = (competitionId, problemId, userId) => {
-  admin.database().ref(`submissions/${competitionId}/${problemId}/${userId}`).once('value', snapshot => {
-    try {
-      const testcases = snapshot.val()[snapshot.val().length - 1].testcases
-      return testcases // We can send all testcases and not even check if they're done because the post request can unpdate the testcases as pending
-    } catch {
-      return "error"
-    }
-
-  })
+const getUserSubmissionTestcases = async (competitionId, problemId, userId) => {
+  const snapshot = await admin.database().ref(`submissions/${competitionId}/${problemId}/${userId}`).once('value')
+  try {
+    const testcases = snapshot.val()[snapshot.val().length - 1].testcases
+    return testcases // We can send all testcases and not even check if they're done because the post request can unpdate the testcases as pending
+  } catch {
+    return "error"
+  }
 }
 
 // GET /api/submission/:competitionId/:problemId
 // Returns latest results for a user's last submission of a competition problem
-app.get('/submission/:competitionId/:problemId/:userId', (req, res) => {
+app.get('/submission/:competitionId/:problemId/:userId', async (req, res) => {
   const { competitionId, problemId, userId } = req.params
-  output = {}
-  const testcases = getUserSubmissionTestcases(competitionId, problemId, userId);
+  const testcases = await getUserSubmissionTestcases(competitionId, problemId, userId)
   res.send(testcases)
 })
 
-app.post('/submission/:competitionId/:problemId/:userId/', (req, res) => {
+const validateTestcase = (submission, language, stdin, stdout) => {
+  return axios({
+    method: 'POST',
+    url: 'https://' + process.env.RAPIDAPI_ENDPOINT + '/submissions',
+    params: {
+      base64_endcoded: 'true',
+      wait: 'true',
+    },
+    headers: {
+      'content-type': 'application/json',
+      'x-rapidapi-host': process.env.RAPIDAPI_ENDPOINT,
+      'x-rapidapi-key': process.env.RAPIDAPI_KEY
+    },
+    data: {
+      language_id: language,
+      source_code: submission,
+      stdin,
+      expected_output: stdout
+    }
+  })
+}
+
+app.post('/submission/:competitionId/:problemId/:userId/', async (req, res) => {
   const { competitionId, problemId, userId } = req.params
   const { submission, language } = req.body
-  let testcaseCount;
-  admin.database().ref(`problems/${problemId}/testcases`).once('value', snapshot => { // Needed to get size of the database
-    testcaseCount = snapshot.val().length
-    const newSubmission = {
-      submission: submission,
-      language: language,
-      time: Date.now(),
-      testcases: Array(testcaseCount).fill("PENDING")
+
+  // First make sure there are no pending submissions
+  const userSubmissionResults = await getUserSubmissionTestcases(competitionId, problemId, userId);
+  if (userSubmissionResults != 'error') {
+    let foundPending = false
+    for (let tc of userSubmissionResults) {
+      if (tc == "PENDING") {
+        foundPending = true
+      }
     }
-    admin.database().ref(`submissions/${competitionId}/${problemId}/${userId}`).once('value', snapshot => { // Needed to get size of the database
-      let index = !!snapshot.val() ? snapshot.val().length : 0
-      admin.database().ref(`submissions/${competitionId}/${problemId}/${userId}`).child(index).set(newSubmission).then(() => {
-        // TODO: Code to update submissions here
-        res.send('Request Sent')
-      }).catch(() => {
-        res.send("Request not Sent")
-      })
-    }).catch(() => {
-      res.send("Request not Sent")
-    })
-  })
+    if (foundPending) {
+      res.send("Currently have pending submission")
+      return
+    }
+  }
+
+  // Get tc data from firebase
+  const testcaseSnapshot = await admin.database().ref(`problems/${problemId}/testcases`).once('value')
+  const testcases = testcaseSnapshot.val()
+  // TODO: handle error if no tc data from firebase 
+  const submissionsSnapshot = await admin.database().ref(`submissions/${competitionId}/${problemId}/${userId}`).once('value')
+  let index = !!submissionsSnapshot.val() ? submissionsSnapshot.val().length : 0
+  let newSubmission = {
+    submission: submission,
+    language: language,
+    time: Date.now(),
+    testcases: Array(testcases.length).fill("PENDING")
+  }
+  await admin.database().ref(`submissions/${competitionId}/${problemId}/${userId}`).child(index).set(newSubmission)
+
+  // TODO: make it so that each test case will update independently 
+  let promises = testcases.map(({ input, output }) => validateTestcase(submission, language, input, output));
+  const results = await Promise.all(promises)
+  const testcaseResults = results.map(result => result.data.status.description)
+
+  newSubmission = { ...newSubmission, testcases: testcaseResults }
+  await admin.database().ref(`submissions/${competitionId}/${problemId}/${userId}`).child(index).set(newSubmission)
+
+  // res.send("tescase sent!")
+  res.send(testcaseResults) // We can do this, I don't have an issue with it as firebase functions last 60 seconds and we can wait tbh
 })
 
 exports.api = functions.https.onRequest(app)
