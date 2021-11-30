3db8938acb0acb51d5282c4028be0cea910dd9dc
hydrol0x <mrjacobry@gmail.com>
2021-11-30 15:50:07 -0500

    Simple homepage adjustments

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Card, Image } from 'react-bootstrap'
 
 import Header from "../components/Header";
 
@@ -6,36 +6,32 @@ export default function Homepage() {
   return (
     <>
       <Header />
-      <Container>
-        <Row>
-          <Col sm={12} lg={8}>
-            <h1> December Inter-School Competition</h1>
-          </Col>
-        </Row>
-        <Row>
-          <p>
-            The Suncoast CS Club December Inter-School Competition is a high school programming contest for students in grades 9-12. The programming contest is modeled after USA Computing Olympiad, and the UCF High School Programming Contest. The contest will require students to use their understandings of logic, algorithms, and data structures to solve programming puzzles.
-          </p>
-        </Row>
-        <Row>
-          <Col sm={12} lg={8}>
-            <h3>Details</h3>
-          </Col>
-          <p>
-            The Suncoast CS Club December Inter-School Competition is a high school programming contest for students in grades 9-12. The programming contest is modeled after USA Computing Olympiad, and the UCF High School Programming Contest. The contest will require students to use their understandings of logic, algorithms, and data structures to solve programming puzzles.
-          </p>
-        </Row>
-        <Row>
-          <Col sm={12} lg={8}>
-            <h3>Register</h3>
-          </Col>
-        </Row>
-        <Row>
-          <p>
-            The Suncoast CS Club December Inter-School Competition is a high school programming contest for students in grades 9-12. The programming contest is modelled after USA Computing Olympiad, and the UCF High School Programming Contest. The contest will require students to use their understandings of logic, algorithms, and data structures to solve programming puzzles.
-          </p>
-        </Row>
-        <Button>Register</Button>
+
+      <Container fluid className="py-3 my-2">
+        <Card >  
+          <Card.Body className="px-4">
+            <Row>
+              <Col sm={12} lg={8}>
+                <h1> December Inter-School Competition</h1>
+              </Col>
+            </Row>
+            <Row>
+              <p>
+                The Suncoast CS Club December Inter-School Competition is a high school programming contest for students in grades 9-12. The programming contest is modeled after USA Computing Olympiad, and the UCF High School Programming Contest. The contest will require students to use their understandings of logic, algorithms, and data structures to solve programming puzzles.
+              </p>
+            </Row>
+            <Row>
+              <Col sm={12} lg={8}>
+                <h3>Details</h3>
+              </Col>
+              <p>
+                The Suncoast CS Club December Inter-School Competition is a high school programming contest for students in grades 9-12. The programming contest is modeled after USA Computing Olympiad, and the UCF High School Programming Contest. The contest will require students to use their understandings of logic, algorithms, and data structures to solve programming puzzles.
+              </p>
+            </Row> 
+          </Card.Body>
+           <Button className="m-2">Register</Button>
+        </Card>
+        
       </Container>
     </>
   )
