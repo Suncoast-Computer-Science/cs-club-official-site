58b84d9d12a51aa0feb48c77a1728070077120e7
hydrol0x <mrjacobry@gmail.com>
2021-12-02 09:06:09 -0500

    basic changes to comeptition cards

--- src/components/CompetitionCard.js
+++ src/components/CompetitionCard.js
@@ -2,8 +2,7 @@ import { Card, Button } from 'react-bootstrap'
 
 export default function CompetitionCard({ name, date, about, link }) {
   return (
-    <Card>
-      <Card.Header>{date}</Card.Header>
+    <Card className="m-2 border-primary">
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>
@@ -11,6 +10,7 @@ export default function CompetitionCard({ name, date, about, link }) {
         </Card.Text>
         <Button variant="primary" href={link}>Learn More</Button>
       </Card.Body>
+      <Card.Footer className="text-muted">Register by: {date}</Card.Footer>
     </Card>
   )
 }
