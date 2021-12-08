b9e77e9c2ffbd0ebb74eed657d818785e49480a9
Rohit Dasgupta <rdg922@gmail.com>
2021-12-08 14:23:47 -0500

    Get Last Submission Backend Request

--- src/api/BackendRequests.js
+++ src/api/BackendRequests.js
@@ -13,3 +13,7 @@ export const gradeSubmission = async (submission, language, competitionId, probl
   })
   return res.data
 }
+
+export const getLastSubmission = async (submission, language, competitionId, problemId, userId) => {
+  const res = await axios.get(process.env.REACT_APP_FIREBASE_FUNCTION_ENDPOINT + `/submission/${competitionId}/${problemId}/${userId}`)
+}
