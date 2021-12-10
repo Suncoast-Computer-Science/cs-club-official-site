21a7371b60bb32d9a748992e6c9edfe14db396de
Rohit Dasgupta <rdg922@gmail.com>
2021-12-09 23:42:52 -0500

    Problem Homepage fully functional

---
+++ src/components/ProblemHomepageHeader.js
@@ -0,0 +1,20 @@
+export default function({ problemData }) {
+  if (problemData) {
+    return (
+      <>
+        <div className="container-fluid bg-light text-dark p-2" >
+          <div class="container bg-light p-4">
+            <h1 class="display-4">{problemData.name}</h1>
+            <p>{problemData.preview}</p>
+          </div>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <p>
+        loading
+      </p>
+    )
+  }
+}
--- src/pages/ProblemHomepage.js
+++ src/pages/ProblemHomepage.js
@@ -1,10 +1,13 @@
 import { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { useAuth } from '../api/AuthContext'
+import { Modal, Table } from 'react-bootstrap';
 import MonacoEditor from 'react-monaco-editor';
 
 import Header from '../components/Header'
 import { testSubmission, gradeSubmission, getLastSubmission } from '../api/BackendRequests'
+
+import ProblemHomepageHeader from '../components/ProblemHomepageHeader'
 
 export default function ProblemHomepage() {
   const { db, currentUser } = useAuth()
@@ -13,10 +16,23 @@ export default function ProblemHomepage() {
   const sampleInputRef = useRef();
 
   const [problemData, setProblemData] = useState(null)
-  const [userProblemData, setUserProblemData] = useState(null)
   const [testResponse, setTestResponse] = useState("")
   const [languageId, setLanguageId] = useState(71)
   const [userCode, setUserCode] = useState("print('bruh')")
+  const [lastSubmissionData, setLastSubmissionData] = useState(null)
+  const [showSubmissionResults, setShowSubmissionResults] = useState(false)
+
+
+  const handleClose = () => setShowSubmissionResults(false)
+  const handleShow = async (e) => {
+    e.preventDefault()
+    setShowSubmissionResults(true)
+    if (currentUser?.uid) {
+      const lastSubmissionRequest = await getLastSubmission(competitionId, problemId, currentUser.uid)
+      // setTestResponse(lastSubmissionRequest.data) // Gets you the last submission results in an array if there is a submission, or 'No Last Submission Found' if there is no last submission
+      setLastSubmissionData(lastSubmissionRequest.data)
+    }
+  }
 
   const [isProcessing, setIsProcessing] = useState(false) // TODO: Render differently if currently processing a request
 
@@ -31,7 +47,8 @@ export default function ProblemHomepage() {
   useEffect(async () => {
     if (currentUser?.uid) {
       const lastSubmissionRequest = await getLastSubmission(competitionId, problemId, currentUser.uid)
-      setTestResponse(lastSubmissionRequest.data) // Gets you the last submission results in an array if there is a submission, or 'No Last Submission Found' if there is no last submission
+      // setTestResponse(lastSubmissionRequest.data) // Gets you the last submission results in an array if there is a submission, or 'No Last Submission Found' if there is no last submission
+      setLastSubmissionData(lastSubmissionRequest.data)
     }
 
   }, [problemData])
@@ -56,7 +73,8 @@ export default function ProblemHomepage() {
       setIsProcessing(true)
       // console.log(currentUser)
       const response = await gradeSubmission(userCode, languageId, competitionId, problemId, currentUser.uid) // Returns an array with all the test case Results
-      setTestResponse(response.toString()) // Do something else
+      // setTestResponse(response.toString()) // Do something else
+      setLastSubmissionData(lastSubmissionData.data)
       setIsProcessing(false)
     }
   }
@@ -64,24 +82,10 @@ export default function ProblemHomepage() {
   return (
     <>
       <Header />
-      {
-        problemData ?
-          <>
-            <div className="container-fluid bg-light text-dark p-2" >
-              <div class="container bg-light p-4">
-                <h1 class="display-4">{problemData.name}</h1>
-                <p>{problemData.preview}</p>
-              </div>
-            </div>
-          </>
-          :
-          <>
-            Loading
-          </>
-      }
-      <div class="container-fluid p-4">
+      <ProblemHomepageHeader problemData={problemData} />
+      <div class="container-fluid p-4" >
         <div class="row">
-          <div class="overflow-auto col-4">
+          <div class="col-4" style={{ height: "100%", overflowY: "hidden" }}>
             {problemData ?
               <>
                 <h2>Problem Statement</h2>
@@ -95,20 +99,30 @@ export default function ProblemHomepage() {
 
                 {/*<p>{JSON.stringify(problemData)}</p> will show you everything available*/}
                 <h3>Samples</h3>
-                {problemData.samples.map(({ input, output }, i) =>
+                {problemData.samples.map(({ input, output, explanation }, i) =>
                   <>
-                    <div class="card">
+                    <div class="card mb-2">
                       <h5 class="card-header">Sample #{i + 1}:</h5>
-                      <div class="card-body">
-                        <p>Input: </p>
-                        {input ?
-                          <p class="card-text bg-dark text-light p-1">{input}</p>
-                          :
-                          <p><i>No Input!</i></p>
-                        }
-                        <p>Output: </p>
-                        <p class="card-text bg-dark text-light p-1">{output}</p>
+                      <div class="card-body container">
+                        <div class="row">
+                          <div class="col">
+                            <p>Input: </p>
+                            {input ?
+                              <p class="card-text bg-dark text-light p-1">{input}</p>
+                              :
+                              <p><i>No Input!</i></p>
+                            }
+                          </div>
+                          <div class="col">
+                            <p>Output: </p>
+                            <p class="card-text bg-dark text-light p-1">{output}</p>
+                          </div>
+                        </div>
                       </div>
+                      {explanation ?
+                        <>
+                          <p>Explanation: {explanation}</p>
+                        </> : <> </>}
                     </div>
                   </>)}
 
@@ -118,39 +132,107 @@ export default function ProblemHomepage() {
             }
 
           </div>
-          <div class="overflow-auto col-8">
+          <div className=" col-8">
+            <form>
+              <div className="form-group row">
+                <label className="col-sm-1 offset-sm-9 col-form-label">
+                  Language:
+                </label>
+                <div className="col-sm-2">
+                  <select className="form-control" value={languageId} onChange={(e) => setLanguageId(e.target.value)}>
+                    <option selected value="71">Python</option>
+                    <option selected value="76">C++</option>
+                    <option selected value="50">C</option>
+                    <option selected value="62">Java</option>
+                  </select>
+                </div>
+              </div>
+            </form>
             <MonacoEditor
               width="100%"
               height="600"
               theme="vs-dark"
-              value={`console.log('bruh')`}
               language='javascript'
               value={userCode}
               onChange={(e) => setUserCode(e)}
             />
-            <form>
-              <label>
-                Pick Your language
-                <select value={languageId} onChange={(e) => setLanguageId(e.target.value)}>
-                  <option selected value="71">Python</option>
-                  <option selected value="76">C++</option>
-                  <option selected value="50">C</option>
-                  <option selected value="62">Java</option>
-                </select>
-              </label>
-              <p>Sample Input: </p>
-              <textarea ref={sampleInputRef}></textarea>
-              <textarea value={testResponse} disabled></textarea>
-              <button onClick={onTestSubmit}>Try Sample</button>
-              {currentUser ?
-                <button onClick={onGradeSubmit}>Submit for Grading</button>
-                :
-                <button disabled> Sign in to Submit! </button>
-              }
-            </form>
+            {!isProcessing ?
+              <form className="form-group row" style={{ overflowY: "hidden" }}>
+                <div className="col-sm-4">
+                  <p className="col-form-label">Test Input: </p>
+                  <textarea className="form-control" style={{ height: "100%" }} ref={sampleInputRef}></textarea>
+                </div>
+                <div className="col-sm-4">
+                  <p className="col-form-label col-sm-2">Output: </p>
+                  <textarea className="form-control" style={{ height: "100%" }} value={testResponse} readonly></textarea>
+                </div>
+                <div className="col-sm-4">
+                  <div className="col-form-label">
+                    &nbsp;
+                  </div>
+                  <div className="py-1">
+                    <button className="btn btn-secondary" onClick={onTestSubmit}>Try Sample</button>
+                  </div>
+                  <div className="py-1">
+                    {currentUser ?
+                      <button className="btn btn-primary" onClick={onGradeSubmit}>Submit for Grading</button>
+                      :
+                      <button className="btn btn-primary" disabled> Sign in to Submit! </button>
+                    }
+                  </div>
+                  <div className="py-1">
+                    <button
+                      className="btn btn-success"
+                      onClick={handleShow}
+                    >
+                      Open Submission Results
+                    </button>
+                  </div>
+                </div>
+              </form>
+              :
+              <p> loading </p>
+            }
           </div>
         </div>
       </div>
+      <Modal show={showSubmissionResults} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Last Submission Results:</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {currentUser?.uid && lastSubmissionData ?
+            <>
+              {typeof (lastSubmissionData) == 'string' ?
+                <>No past submissions!</>
+                :
+                <>
+                  <thead>
+                    <tr>
+                      {lastSubmissionData.map((_, index) => (
+                        <th key={index}>{index}</th>
+                      ))} </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      {lastSubmissionData.map((result, index) => (
+                        <td key={index}>{result}</td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </>
+              }
+            </>
+            :
+            <>Sign in to view Submission Results!</>
+          }
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-secondary" onClick={handleClose}>
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
