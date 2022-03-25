85d11863d918a13493e7bbee530192eee1967a77
Rohit Dasgupta <rdg922@gmail.com>
2022-03-25 10:08:44 -0400

    Remove Admin panel worked on in other Branch

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,21 +1,8 @@
 import Header from "../components/Header";
-import AddCompetitionPanel from "../components/AddCompetitionPanel";
-import AddProblemPanel from "../components/AddProblemPanel";
-
 const Admin = () => {
   return (
     <>
       <Header />
-      <div className="container-fluid bg-light text-dark p-2">
-        <div className="container bg-light p-4">
-          <h1 className="display-4">Admin Panel</h1>
-          <h4>Make, edit, and see results for your competitions</h4>
-        </div>
-      </div>
-      <div className="container">
-        <AddCompetitionPanel />
-        <AddProblemPanel />
-      </div>
     </>
   );
 };
