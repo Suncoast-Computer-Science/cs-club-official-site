e2ed02a0c207f0e6ce6c2db4a436d1771f78ad4f
hydrol0x <mrjacobry@gmail.com>
2022-11-09 15:16:24 -0500

    Main frontend of register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -8,6 +8,8 @@ import {
 	Row,
 	Col,
 	Alert,
+	OverlayTrigger,
+	Tooltip,
 } from 'react-bootstrap';
 import Header from '../components/Header';
 
@@ -15,22 +17,31 @@ import { useAuth } from '../api/AuthContext';
 
 export default function Signin() {
 	const navigate = useNavigate();
-	const { register } = useAuth();
+	const { register, currentUser } = useAuth();
 	const [errorMessage, setErrorMessage] = useState(null);
 	const emailRef = useRef(null);
-	const passwordRef = useRef(null);
+	const nameRef = useRef(null);
 
 	async function handleEvent(e) {
 		e.preventDefault();
 		const email = emailRef.current.value;
-		const password = passwordRef.current.value;
-		const error = await register(email, password);
+		const name = nameRef.current.value;
+		const error = await register(email, name);
 
 		if (error) {
 			setErrorMessage('Error: ' + error.code.slice(5));
 		} else {
 			navigate('/');
 		}
+	}
+
+	let displayName;
+	let schoolEmail;
+	let userId;
+	if (currentUser) {
+		displayName = currentUser.displayName;
+		schoolEmail = currentUser.email;
+		userId = currentUser.uid;
 	}
 
 	return (
@@ -40,41 +51,96 @@ export default function Signin() {
 			<Container>
 				<Row>
 					<Col>
-						<Card className='mx-auto mt-5' style={{ width: '25rem' }}>
+						<Card
+							className='mx-auto mt-5'
+							style={{ width: '50rem', height: '40rem' }}
+						>
 							<Card.Header as='h4' className='text-center'>
-								Create your account
+								Register
 							</Card.Header>
 							<Card.Body>
+								<Card.Title className='text-center'>
+									Confirm your information
+								</Card.Title>
 								<Alert hidden={!errorMessage} variant='warning'>
 									{errorMessage}
 								</Alert>
+
 								<Form>
-									<Form.Group className='mb-3' controlId='formGroupEmail'>
-										<Form.Label>Email address</Form.Label>
+									<Form.Group className='mb-3' controlId='formGroupName'>
+										<Form.Label>Full Name</Form.Label>
+										<Form.Control
+											type='text'
+											ref={nameRef}
+											placeholder='Name'
+											defaultValue={displayName}
+										/>
+									</Form.Group>
+									<Form.Group className='mb-3' controlId='formGroupSchoolEmail'>
+										<Form.Label>School Email address</Form.Label>
+										<Form.Control
+											disabled
+											type='email'
+											ref={emailRef}
+											placeholder='Enter school email'
+											defaultValue={schoolEmail}
+										/>
+									</Form.Group>
+									<Form.Group
+										className='mb-3'
+										controlId='formGroupPersonalEmail'
+									>
+										<OverlayTrigger
+											placement='right'
+											delay={{ show: 250, hide: 400 }}
+											overlay={(props) => (
+												<Tooltip {...props}>
+													So we can email you without getting blocked!
+												</Tooltip>
+											)}
+										>
+											<Form.Label>
+												Personal Email Address<b> (?) </b>
+											</Form.Label>
+										</OverlayTrigger>
 										<Form.Control
 											type='email'
 											ref={emailRef}
-											placeholder='Enter email'
+											placeholder='Enter school email'
+											defaultValue={schoolEmail}
 										/>
 									</Form.Group>
-									<Form.Group className='mb-3' controlId='formGroupPassword'>
-										<Form.Label>Password</Form.Label>
-										<Form.Control
-											type='password'
-											ref={passwordRef}
-											placeholder='Password'
-										/>
+									<Form.Group></Form.Group>
+									<Form.Group className='mb-3' controlId='formGroupGrade'>
+										<Form.Label>Grade level</Form.Label>
+										<Form.Select>
+											{[
+												'Select Grade',
+												'9th',
+												'10th',
+												'11th',
+												'12th',
+												'Other',
+											].map((grade) => (
+												<option value={grade}>{grade}</option>
+											))}
+										</Form.Select>
+										<div className='mt-4'>
+											<Form.Check className='d-inline' type='switch' />
+											<p className='d-inline mx-2'>
+												Agree to recieve emails from Suncoast CS Club
+											</p>
+										</div>
 									</Form.Group>
 								</Form>
 								<Button variant='primary' onClick={handleEvent} type='submit'>
-									{' '}
-									Register{' '}
+									Confirm
 								</Button>
 							</Card.Body>
+							{/*  TODO: implement log out  */}
 							<Card.Footer>
 								<Card.Link href='/signin' style={{ textDecoration: 'none' }}>
-									{' '}
-									Have an Account? Sign In.{' '}
+									Don't want to register? Log out.
 								</Card.Link>
 							</Card.Footer>
 						</Card>
